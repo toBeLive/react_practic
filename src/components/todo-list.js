0b32ceeba261f3b93499740c8bd9cce2e827fd16
mrTexas <tolivedp@gmail.com>
2019-01-22 19:33:29 +0200

    step 2

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -8,9 +8,12 @@ const currentLanguage = Language.langRu;
 const TodoList = ({ todos }) => {
 
     const elements = todos.map((item) => {
+
+        const { id, ...itemProps } = item;
+
         return (
-            <li key = { item.id }>
-                <TodoListItem { ...item } />
+            <li key = { id }>
+                <TodoListItem { ...itemProps } />
             </li>
         );
     });
